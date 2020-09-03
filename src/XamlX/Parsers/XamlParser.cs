53e6375d211af1ccddef04eac99e5022b675a0ef
Przemysław Onak <przemekonak@gmail.com>
2020-09-03 22:49:37 +0200

    Parse namespaces with GuiLabs parser

---
+++ src/XamlX/Parsers/XamlParser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using XamlX.Ast;
+
+namespace XamlX.Parsers
+{
+    public static class XamlParser
+    {
+        public static XamlDocument Parse(string s, Dictionary<string, string> compatibilityMappings = null)
+        {
+            return GuiLabsXamlParser.Parse(s, compatibilityMappings);
+        }
+        public static XamlDocument Parse(TextReader reader, Dictionary<string, string> compatibilityMappings = null)
+        {
+            return GuiLabsXamlParser.Parse(reader, compatibilityMappings);
+        }
+    }
+}
